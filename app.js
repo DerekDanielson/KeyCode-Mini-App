16bbe68f8edcc359e0bd1810cf79b1d2eed5fe29
Derek <derekdanielson84@gmail.com>
2023-05-18 10:45:15 -0600

    Completed JavaScript

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 window.addEventListener('keydown', showKeyCodes);
 
 function showKeyCodes(e){
+    // Select insert
     const insert = document.getElementById('insert');
-
+    insert.innerHTML = '';
+    
+    // Create object
     const keyCodes = {
         'e.key': e.key === ' ' ? 'Space' : e.key,
         'e.keyCode': e.keyCode,
         'e.code': e.code
     }
-    console.log(keyCodes);
+    
+    for( let key in keyCodes){
+        // Create divs    
+        const div = document.createElement('div');
+        div.className = 'key';
+        const small = document.createElement('small');
+        
+        // Create text nodes
+        const keyText = document.createTextNode(key);
+        const valueText = document.createTextNode(keyCodes[key]);
+
+        // Append text nodes to elements
+        small.appendChild(keyText);
+        div.appendChild(valueText);
+        div.appendChild(small);
+
+        //Add div to dom
+        insert.appendChild(div);
+    }
 }
